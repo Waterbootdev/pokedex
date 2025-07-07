1cdefa46d8ca751e441bc84c4e2acaa8d353bd1a
Waterbootdev <wassermann.sp@gmail.com>
2025-07-07 10:21:12 +0200

    repl

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func PrintPrompt() {
+	fmt.Print("Pokedex > ")
+}
+
+func ScanCleanInput() []string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return CleanInput(scanner.Text())
+}
+
+func PrintCommand(cleanInput []string) {
+	command := ""
+	if len(cleanInput) > 0 {
+		command = cleanInput[0]
+	}
+	fmt.Printf("Your command was: %v\n", command)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	for {
+		PrintPrompt()
+		cleanInput := ScanCleanInput()
+		PrintCommand(cleanInput)
+	}
 }
